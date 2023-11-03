177d3d78739f6b2e6ab89f7de75c7a0f6f71d220
Asadbek_Khoshimov <84382619+asadbekkhoshimov@users.noreply.github.com>
2023-11-03 11:20:26 +0900

    lane_detection

--- lane_detection_project2/lane_detection.py/lane_detection.py
+++ lane_detection_project2/lane_detection.py/lane_detection.py
@@ -1,26 +1,29 @@
 import cv2
+import numpy as np
 
+# Video capture
 video = cv2.VideoCapture("/Users/asadbekkhoshimov/Desktop/Reaserach-Program/lane_detection_project2/road_car_view.mp4")
 
 while True:
-    ret, frame = video.read()
+    ret, or_frame=video.read()
     if not ret:
         video = cv2.VideoCapture("/Users/asadbekkhoshimov/Desktop/Reaserach-Program/lane_detection_project2/road_car_view.mp4")
         continue
-
-    # Convert the frame to grayscale for edge detection
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    # Apply Canny edge detection
-    edges = cv2.Canny(gray_frame, 50, 150)
-
-    # Create a blank frame to display both the original and edges
-    combined_frame = cv2.hconcat([frame, cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)])
-
-    cv2.imshow("Original and Edges", combined_frame)
-
-    key = cv2.waitKey(25)
-    if key == 27:
+    frame = cv2.GaussianBlur(or_frame,(5,5),0)
+    hsv=cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)
+    
+    lower_y=np.array([18,94,140])
+    upper_y=np.array([48,255,255])
+    
+    mask=cv2.inRange(hsv,lower_y,upper_y)
+    edges=cv2.Canny(mask,74,150)
+    lines=cv2.HoughLinesP(edges,1,np.pi/180,50,maxLineGap=50)
+    if lines is not None:
+        for line in lines:
+            x1,y1,x2,y2=line[0]
+            cv2.line(frame,(x1,y1),(x2,y2),(0,255,0),5)
+    cv2.imshow("frame",frame)
+    cv2.imshow("edges",edges)
+    key=cv2.waitKey(25)
+    if(key==27):
         break
-
-cv2.destroyAllWindows()
